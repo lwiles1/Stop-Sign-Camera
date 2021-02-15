3f2d15eafc1096d89cbc76540d2df6c9406ec984
chadrans <50344494+chadrans@users.noreply.github.com>
2021-02-15 14:19:22 -0500

    Create platedetection.py

---
+++ platedetection.py
@@ -0,0 +1,9 @@
+import cv2
+import imutils
+
+#read the image into cv object
+#Resize on standardized license plate zone (later)
+#Greyscale the image
+#blur the image
+#Perform edge detection
+#Looking for contours
