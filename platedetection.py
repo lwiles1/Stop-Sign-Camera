fa7cfb30b418c69a7b308ffd71f2d90d24d5f78d
chadrans <50344494+chadrans@users.noreply.github.com>
2021-02-16 16:58:38 -0500

    Update platedetection.py
    Changed window names

--- platedetection.py
+++ platedetection.py
@@ -11,18 +11,18 @@ cv2.waitKey()
 #Resize on standardized license plate zone (later)
 #Greyscale the image
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)     #https://docs.opencv.org/3.4/d8/d01/group__imgproc__color__conversions.html
-cv2.imshow("Gray", image)
+cv2.imshow("Gray-Scale", image)
 cv2.waitKey()
 
 #blur the image
 image = cv2.bilateralFilter(image, 5, 60, 60)      #https://docs.opencv.org/master/d4/d86/group__imgproc__filter.html
-cv2.imshow("Blurred", image)                       # Numbers determined from docs above
+cv2.imshow("Bilateral Filter", image)                       # Numbers determined from docs above
 cv2.waitKey()
 #Efficiency Possibility: 5 is the recommended for real-time systems, but this could be reduced
 
 #Perform edge detection
 image = cv2.Canny(image, 100, 200)          #https://docs.opencv.org/3.4/da/d5c/tutorial_canny_detector.html
-cv2.imshow("Edges", image)
+cv2.imshow("Canny Edge Detection", image)
 cv2.waitKey()
 #Efficiency Possibility: Write my own Canny algorithm
 
